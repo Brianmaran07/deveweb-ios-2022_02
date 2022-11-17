11b6aee7b27f8ae71da993e7d6a85f7877fe1321
vitorleony <vleony953@gmail.com>
2022-11-17 19:45:26 -0300

    subida javascript

---
+++ react/tema_01_exemplo_01/src/components/Header.jsx
@@ -0,0 +1,18 @@
+ import PropTypes from 'prop-types';
+const Header = ({ title }) => {
+return (
+<header>
+<h1>Agenda de {title}</h1>
+</header>
+);
+};
+
+ Header.defaultProps = {
+ title: 'compromissos',
+ };
+
+ Header.propTypes = {
+ title: PropTypes.string.isRequired,
+ };
+
+ export default Header;
